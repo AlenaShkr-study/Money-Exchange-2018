94a390e6353dcf8a944b21b5f9969c767e6f4608
AlenaShkr <elalshkr@gmail.com>
2018-09-26 12:15:42 +0300

    Money-Exchange: implement method of counting coins by value

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
-// PLEASE DON'T change function name
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+   let countcoins={};
+    if (currency>10000){
+    countcoins={
+        error: "You are rich, my friend! We don't have so much coins for exchange"}
+       }
+    else if (currency===0 | currency<0)
+    countcoins; 
+    else {
+    const H=50;
+    const Q=25;
+    const D=10;
+    const N=5;
+    const P=1;
+
+    countcoins.H=currency/H|0;
+    currency=currency%H;
+    countcoins.Q=currency/Q|0;
+    currency=currency%Q;
+    countcoins.D=currency/D|0;
+    currency=currency%D; 
+    countcoins.N=currency/N|0;
+    currency=currency%N;
+    countcoins.P=currency;
+    
+    for (let key in countcoins){
+        if (countcoins[key]==0){
+        delete countcoins[key];
+        } 
+    }  
 }
+return countcoins;
+}
+
+     
+
